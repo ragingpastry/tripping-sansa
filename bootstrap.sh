29e15e131482b8b4dd0b7a4ef5a4d54b185d80d0
Nick Wilburn <senior.crepe@gmail.com>
2015-08-01 17:31:36 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ function syncFiles() {
     rsync --exclude ".git/" --exclude "boostrap.sh" --exclude "README.md" -avh --no-perms ~/.dotfiles/ ~;
 }
 
-if [[ "$1" == "--force" -o "$1" == "-f" ]]; then
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
     syncFiles;
 else
     read -p "This may be overwriting existing files. Are you sure? (y/n)" -n 1;
